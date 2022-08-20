014150a1253c3ef43be6565c250acd163c3c9f99
Yaroslav Kukytsyak <yarokuk@gmail.com>
2022-08-20 18:47:58 +0200

    feat: add ~ path alias

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable @typescript-eslint/naming-convention */
 import type { Config } from '@jest/types'
+import { pathsToModuleNameMapper } from 'ts-jest'
+import tsconfig from './tsconfig.base.json'
 
 const config: Config.InitialOptions = {
   globals: {
@@ -13,6 +16,9 @@ const config: Config.InitialOptions = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
   collectCoverageFrom: ['src/**/*.ts?(x)', '!src/**/*.d.ts'],
 }
 
